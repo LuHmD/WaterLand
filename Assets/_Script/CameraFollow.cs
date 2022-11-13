9400bf4ac91a1749cecf28add7659681510ebaf6
Chenwie07 <kieradharke@gmail.com>
2022-11-13 04:36:59 +0100

    2 levels templates set

--- Assets/_Script/CameraFollow.cs
+++ Assets/_Script/CameraFollow.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform m_FollowTarget;
-    public Vector3 offset = new Vector3(4, 1.5f, -8f); 
+    public Vector3 offset; 
 
-    public bool followVertical = false; 
+    public bool followVertical = false;
+
+    private void Start()
+    {
+        transform.position = m_FollowTarget.position; 
+    }
     private void LateUpdate()
     {
-        transform.position = m_FollowTarget.position + offset;
+        var targetPos = m_FollowTarget.position + offset;
+        transform.position = targetPos; 
         if (!followVertical)
-            transform.position = new Vector3(m_FollowTarget.position.x, 2.5f, m_FollowTarget.position.z) + offset;     
+            transform.position = new Vector3(m_FollowTarget.position.x,9.5f, m_FollowTarget.position.z) + offset;     
     }
 }
